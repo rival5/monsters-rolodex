b6ba0d793c1c3036cd7bfd416d0bbde683b68497
rival5 <viralraval5@gmail.com>
2020-01-01 20:37:50 +0530

    Updates

---
+++ precache-manifest.5fd9c5121090116943826e48b22d8d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99c78c47321e10814abd179884af26b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2a99af7f49a6a7aa1029",
+    "url": "/monsters-rolodex/static/css/main.e2db8c59.chunk.css"
+  },
+  {
+    "revision": "5c6ffb1144ca72e136ae",
+    "url": "/monsters-rolodex/static/js/2.243d5167.chunk.js"
+  },
+  {
+    "revision": "2a99af7f49a6a7aa1029",
+    "url": "/monsters-rolodex/static/js/main.8a41e5db.chunk.js"
+  },
+  {
+    "revision": "6d330d82999b0c84dbb7",
+    "url": "/monsters-rolodex/static/js/runtime~main.55ecfcda.js"
+  }
+]);
