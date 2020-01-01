a02edffa1f19e017c49f8154569a5113181fbb55
rival5 <viralraval5@gmail.com>
2020-01-01 20:32:20 +0530

    styling and font

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
